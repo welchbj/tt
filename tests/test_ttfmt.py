78585c783013c6f06f7e20eee6a654759b70e99c
welchbj <welch18@vt.edu>
2015-11-24 21:01:00 -0500

    Add basic tests for ttfmt get_vars method

--- tests/test_ttfmt.py
+++ tests/test_ttfmt.py
@@ -1,9 +1,18 @@
 import unittest
 
+import tt.fmttools.ttfmt as ttfmt
+
 class TestTtFmt(unittest.TestCase):
 
-    def testName(self):
-        pass
+    def test_get_vars(self):
+        data_provider = {
+                         # Simple test cases
+                         "F = A and B" : ["F", "A", "B"],
+                         "F = A and B or C" : ["F", "A", "B", "C"],
+                         }
+        
+        for eq in data_provider:
+            self.assertListEqual(data_provider[eq], ttfmt.get_vars(eq))
 
 if __name__ == "__main__":
     unittest.main()
