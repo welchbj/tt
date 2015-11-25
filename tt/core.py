d89c9d56820c3ea3710384fdd790fd852cf99636
welchbj <welch18@vt.edu>
2015-11-24 19:15:30 -0500

    Fix --help option formatting

--- tt/core.py
+++ tt/core.py
@@ -1,23 +1,24 @@
 import sys
 import os
 
-from argparse import ArgumentParser, ArgumentError
-from argparse import RawDescriptionHelpFormatter
+from argparse import ArgumentParser, RawTextHelpFormatter
 
 __all__ = ['main']
 __version__ = 0.1
 __date__ = "2015-11-24"
 __updated__ = "2015-11-24"
 
-def help_provider():
-    pass
-
 def main(argv=None):
+    """The main entry point for the program. Arguments and arguments 
+        descriptions are defined, and the correct functionality is chosen
+        from the user input.    
+    """
     if argv is None:
         argv = sys.argv
     else:
         sys.argv.extend(argv)
 
+    program_source_control_url = "www.github.com/welchbj/tt"
     program_name = os.path.basename(sys.argv[0])
     program_version = "v{version}".format(version=str(__version__))
     program_build_date = str(__updated__)
@@ -38,26 +39,34 @@ def main(argv=None):
     try:
         # Setup argument parser
         parser = ArgumentParser(description=program_desc, 
-                                formatter_class=RawDescriptionHelpFormatter)
+                                formatter_class=RawTextHelpFormatter)
         parser.add_argument("--kmap", 
                             action="store_true", 
-                            help="generate kmap of specified boolean equation")
+                            help="generate kmap of specified boolean equation.")
         parser.add_argument("--intermediates", 
                             action="store_true", 
-                            help="""indicates that intermediate boolean expressions should be displayed with their own column in the truth table. 
-                                    Not valid with the --kmap option.""")
+                            help="indicates that intermediate boolean expressions should be displayed with their own column in the truth table.\n"
+                                  "Not valid with the --kmap option.")
+        parser.add_argument("--version",
+                            action="version",
+                            version=program_version_message,
+                            help="program version and latest build date")
         parser.add_argument(dest="equation", 
-                            help="Boolean equation to be analyzed. This should be the last argument provided") 
+                            help="Boolean equation to be analyzed, surrounded with double quotes.\n"
+                                 "Boolean operations are specified with:\n\n"
+                                 "AND -> &\n"
+                                 "OR -> |\n\n"
+                                 "These are the operations currently supported\n"
+                                 "The equation should be the last argument specified") 
 
         # Process arguments
         args = parser.parse_args()
         kmap = args.kmap
         intermediates = args.intermediates
-        equation = args.intermediates
-        
+        equation = args.equation
         
         if kmap and intermediates:
-            raise ArgumentError("--intermediates option is not compatible with kmap generation")
+            parser.error("--intermediates option is not compatible with kmap generation")
         
         print("equation: {eq}".format(eq=equation))
             
@@ -66,6 +75,8 @@ def main(argv=None):
         return 0
     except Exception as e:
         indent = len(program_name) * " "
+        sys.stderr.write("An unexpected error occurred.")
+        sys.stderr.write("Please notify the project maintainer at {url}".formart(url=program_source_control_url))
         sys.stderr.write(program_name + ": " + repr(e) + "\n")
         sys.stderr.write(indent + "  for help use --help")
         return 2
