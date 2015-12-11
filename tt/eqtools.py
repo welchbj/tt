e8283466978cdbd4a174b539a71e4470b548d417
welchbj <welch18@vt.edu>
2015-12-11 18:18:42 -0500

    Several changes to equation parsing functionality and workflow
    Move schema and associated classes to their own file.
    Add method for Shunting-Yard infix to postfix algorithm.
    Refactor basic method names for more descriptive behavior.

--- tt/eqtools.py
+++ tt/eqtools.py
@@ -1,30 +1,19 @@
 '''
-A module for extracting an manipulating information from Boolean equations.
+A module for extracting and manipulating information from Boolean equations.
 '''
 import logging as log
 
 from tt.utils import without_spaces
-
-eq_transform_sym_dict = {
-                         "~" : ["not", "NOT",  "~", "!"],
-                         "&" : ["and", "AND", "&", "&&", "/\\"],
-                         "|" : ["or", "OR", "|", "||", "\\/"]
-                        }
+from tt.schema_provider import schema
 
 def transform_eq_to_generic_schema(raw_eq):
     '''Receives a user-specified Boolean equation and attempts to transform it
     to the generic Python boolean schema, using keywords and, or, not.
-    
-    Currently unsupported:
-    nand
-    nor
-    xor
-    xnor
     '''
-        
+
     transformed_eq = raw_eq
-    for tt_schema_sym, other_schema_sym_list in eq_transform_sym_dict.items():
-        for sym in other_schema_sym_list:
+    for tt_schema_sym, bool_operator in schema.items(): # TODO: need to update to use new schema
+        for sym in bool_operator.equivalent_symbols:
             if sym in transformed_eq:
                 transformed_eq = transformed_eq.replace(sym, tt_schema_sym)
     return without_spaces(transformed_eq)
@@ -32,7 +21,7 @@ def transform_eq_to_generic_schema(raw_eq):
 def extract_output_sym_and_expr(eq):
     '''Assumes that the equation has already been transformed to tt's generic
     schema for Boolean equations'''
-    
+
     output_and_expr = eq.split("=")
     
     if len(output_and_expr) == 1:
@@ -40,20 +29,53 @@ def extract_output_sym_and_expr(eq):
         raise RuntimeError 
     
     if len(output_and_expr) > 2:
-        log.error("More than one equals sign (\"=\") found in your Boolean equation. Cannot continue program execution")
+        log.error("More than one equals sign (\"=\") found in your Boolean equation. Cannot continue program execution.")
         raise RuntimeError 
     
     output_sym = output_and_expr[0]
-    if not is_valid_symbol(output_sym):
+    if not is_valid_variable_symbol(output_sym):
         log.warning("Output symbol did not meet the recommended guidelines for equation symbols.\n"
-                    "This is probably fine for the output variable, but may cause problems if you followed the same patterns for dependent variable symbols.")
+                    "This is probably fine for the output variable, but may cause problems if you followed the same "
+                    "patterns for dependent variable symbols.")
     
     expr = output_and_expr[1]
     
     return (output_sym, expr)
-        
 
-def is_valid_symbol(sym):
+def infix_to_postfix(infix):
+    # TODO: currently can only handle well-formed expressions
+    stack = []
+    postfix = []
+    operators = schema.keys()
+
+    for c in infix:
+        if is_valid_variable_symbol(c):
+            postfix += c
+        elif c == "(":
+            stack += c
+        elif c in operators:
+            if not stack:
+                stack += c
+            else:
+                while stack[-1] != "(" and schema[stack[-1]] > schema[c]:
+                    postfix += stack.pop()
+                stack += c
+        elif c == ")":
+            while stack and stack[-1] != "(":
+                postfix += stack.pop()
+            stack.pop()
+
+    # TODO: check for remaining parens
+    for c in reversed(stack):
+        postfix += c
+
+    return "".join(postfix)
+
+def transform_postfix_to_primitive_functions(postfix):
+    pass
+
+def is_valid_variable_symbol(sym):
+    # TODO: support for lowercase and checking of numeric symbols
     return len(sym) == 1 and sym.isalnum() and sym.isupper()
 
 def extract_eq_symbols(eq):
@@ -63,16 +85,11 @@ def extract_eq_symbols(eq):
     The first symbols in the list is the result of the equation.
     '''
     # TODO: more advanced logic in determining valid symbols
-    return [sym for sym in eq if is_valid_symbol(sym)]
+    return [sym for sym in eq if is_valid_variable_symbol(sym)]
 
 def extract_eq_intermediates(eq):
     pass
 
 def generate_eq_inputs(symbol_list):
     for sym in symbol_list:
-        pass 
-    
-    
-    
-    
-    
+        pass
---
+++ tt/schema_provider.py
@@ -0,0 +1,69 @@
+from enum import Enum
+
+class BooleanOperator(object):
+
+
+    def __init__(self, precedence_in, bool_func_in, *args):
+        self.precedence = precedence_in
+        self.bool_func = bool_func_in
+        self.equivalent_symbols = list(args)
+
+    def result(self, a, b):
+        return self.bool_func(a, b)
+
+    # define rich-comparison operators
+    def __gt__(self, other):
+        return self.precedence.__gt__(other.precedence)
+    
+    def __ge__(self, other):
+        return self.precedence.__ge__(other.precedence)
+    
+    def __lt__(self, other):
+        return self.precedence.__lt__(other.precedence)
+    
+    def __le__(self, other):
+        return self.precedence.__le__(other.precedence)
+    
+    def __eq__(self, other):
+        return self.precedence.__eq__(other.precedence)
+    
+    def __ne__(self, other):
+        return self.precedence.__ne__(other.precedence)
+
+class Precedence(Enum):
+    ZERO = 0
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
+def bool_not(a, b):
+    return not a
+
+def bool_and(a, b):
+    return a and b
+
+def bool_nand(a, b):
+    return not (a and b)
+
+def bool_xor(a, b):
+    return ((not a) and b) or (a and (not b))
+
+def bool_xnor(a,b):
+    return a == b
+
+def bool_or(a, b):
+    return a or b
+
+def bool_nor(a, b):
+    return not (a or b)
+
+# TODO: possibly allow users to make their own precedence maps in the future
+schema = {
+    "~" : BooleanOperator(Precedence.HIGH, bool_not, "not", "NOT", "~", "!"),
+    "+" : BooleanOperator(Precedence.MEDIUM, bool_xor, "xor", "XOR"),
+    "@" : BooleanOperator(Precedence.MEDIUM, bool_xnor, "xnor", "XNOR", "nxor", "NXOR"),
+    "&" : BooleanOperator(Precedence.LOW, bool_and, "and", "AND", "&", "&&", "/\\"),
+    "$" : BooleanOperator(Precedence.LOW, bool_nand, "nand", "NAND"),
+    "|" : BooleanOperator(Precedence.ZERO, bool_or, "or", "OR", "|", "||", "\\/"),
+    "%" : BooleanOperator(Precedence.ZERO, bool_nor, "nor", "NOR")
+}
