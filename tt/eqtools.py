1c54e1e3033af446d1928254c2eacc106f0bce87
welchbj <welch18@vt.edu>
2015-12-20 11:17:55 -0500

    Add syntax checking of boolen equations.

--- tt/eqtools.py
+++ tt/eqtools.py
@@ -6,40 +6,47 @@ import logging as log
 import itertools
 
 from utils import without_spaces
-from schema_provider import schema, schema_search_ordered_list
+from schema_provider import schema, schema_search_ordered_list, SYM_NOT, SYM_XOR
 
 
 class EvaluationResultWrapper(object):
     def __init__(self, input_symbols, output_symbol):
         self.input_symbols = input_symbols
-        self.output_symbols = output_symbol
+        self.output_symbol = output_symbol
         self.result_list = []
 
     def get_num_evaluations(self):
         return 2**len(self.input_symbols)
 
 
-class BooleanExpressionWrapper(object):
-    def __init__(self, raw_infix_expr):
+class BooleanEquationWrapper(object):
+    def __init__(self, raw_bool_eq):
         self.unique_symbols_left = list(reversed("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
         self.unique_symbol_to_var_name_dict = {}
 
-        self.infix_expr = raw_infix_expr
-        self.condensed_infix_expr = self.condense_expression(raw_infix_expr)
+        self.output_symbol, self.infix_expr = extract_output_sym_and_expr(raw_bool_eq)
+
+        self.condensed_infix_expr = self.condense_expression(self.infix_expr)
         self.postfix_expr = self.infix_to_postfix(self.condensed_infix_expr)
 
-    def get_variable_list(self):
-        return [val for key, val in self.unique_symbol_to_var_name_dict.items()]
+    def get_input_symbol_list(self):
+        """
+        Get the list of input symbols contained within this equation.
+
+        Returns:
+            The list of input symbols of the equation, sorted by their order in the equation.
+        """
+        return [self.unique_symbol_to_var_name_dict[key] for key in self.get_unique_symbol_list()]
 
     def get_unique_symbol_list(self):
-        return self.unique_symbol_to_var_name_dict.keys()
+        return sorted(self.unique_symbol_to_var_name_dict.keys())
 
     def get_unique_symbol(self, var_name):
         if not self.unique_symbols_left:
-            log.fatal("Exhausted all variable symbols in Boolean equation. "
-                      "This means you tried to evaluate an equation of more than 26 variables. "
+            log.error("Exhausted all variable symbols in Boolean equation. "
+                      "This means you tried to evaluate an equation of more than 26 variables, which is not supported. "
                       "Cannot continue program execution.")
-            raise RuntimeError
+            raise RuntimeError # TODO: better exception type?
         elif var_name in self.unique_symbol_to_var_name_dict.values():
             # all values should be unique, so we should be safe to assume this list comprehension will yield one element
             return [key for key, val in self.unique_symbol_to_var_name_dict.items() if val == var_name][0]
@@ -48,25 +55,39 @@ class BooleanExpressionWrapper(object):
             self.unique_symbol_to_var_name_dict[unique_symbol] = var_name
             return unique_symbol
 
+    def get_evaluation_result(self):
+        eval_result_wrapper = EvaluationResultWrapper(self.get_input_symbol_list(), self.output_symbol)
+
+        input_symbols = self.unique_symbol_list()
+        for input_row in get_symbol_input_array(self.input_symbols()):
+            expr_to_eval = replace_inputs(self.postfix_expr, input_symbols, input_row)
+            result = eval_postfix_expr(expr_to_eval)
+            eval_result_wrapper.result_list.append(str(result))
+
+        return eval_result_wrapper
+
     def condense_expression(self, raw_infix_expr):
         # TODO: doc
         """
-        Ensures that the expression is well-formed and transforms variable names to single inputs and converts
-        Boolean operations to single character equivalents defined in the tt schema.
+
+        Args:
+            raw_infix_expr:
 
         Returns:
 
         """
 
         # TODO: need to check for consecutive symbols
-
+        condensed_expr = raw_infix_expr
         curr_idx = 0
         prev_char_is_operand = False
         left_paren_positions = []
         right_paren_positions = []
+        operand_positions = []
+        operation_positions = []
 
-        while curr_idx < len(raw_infix_expr):
-            c = raw_infix_expr[curr_idx].strip()
+        while curr_idx < len(condensed_expr):
+            c = condensed_expr[curr_idx].strip()
             if not c:
                 pass
             elif c == "(":
@@ -80,7 +101,7 @@ class BooleanExpressionWrapper(object):
                     equivalent_symbols = schema[tt_operation_symbol].equivalent_symbols # assume this list is pre-ordered by length in the schema
                     operation_symbol_lens = [len(equivalent_symbol) for equivalent_symbol in equivalent_symbols]
                     test_idxs = [curr_idx+operation_symbol_len for operation_symbol_len in operation_symbol_lens]
-                    possible_matches = [raw_infix_expr[curr_idx:test_idx] for test_idx in test_idxs if test_idx <= len(raw_infix_expr)]
+                    possible_matches = [condensed_expr[curr_idx:test_idx] for test_idx in test_idxs if test_idx <= len(condensed_expr)]
                     matches = [possible_match for operation_symbol, possible_match in zip(equivalent_symbols, possible_matches) if operation_symbol == possible_match]
 
                     if matches:
@@ -88,39 +109,76 @@ class BooleanExpressionWrapper(object):
                         matched_idx = possible_matches.index(match)
                         replacement_idx = test_idxs[matched_idx]
 
-                        if replacement_idx == len(raw_infix_expr):
-                            log.error("Boolean equation ended with an operation. Its last scope must end with an operand.")
-                            raise GrammarException
+                        if replacement_idx < len(condensed_expr):
+                            if match[-1].isalpha() and condensed_expr[replacement_idx] not in [" ", "("]:
+                                # this is just an operand that begins with one of the tt schema symbols;
+                                # we can break out of this because no operation symbols for different operations
+                                # start with another symbol
+                                break
 
-                        if match[-1].isalpha() and raw_infix_expr[replacement_idx] not in [" ", "("]:
-                            # this is just an operand that begins with one of the tt schema symbols;
-                            # we can break out of this because no operation symbols for different operation
-                            # start with another symbol
-                            break
+                        expr_kept_leading = condensed_expr[:curr_idx]
+                        expr_kept_trailing = condensed_expr[replacement_idx:]
+                        if tt_operation_symbol == SYM_NOT:
+                            replacement_symbol = "1" + SYM_XOR
+                            curr_idx += 1
+                        else:
+                            replacement_symbol = tt_operation_symbol
 
-                        raw_infix_expr = raw_infix_expr[:curr_idx] + tt_operation_symbol + raw_infix_expr[replacement_idx:]
+                        condensed_expr = expr_kept_leading + replacement_symbol + expr_kept_trailing
                         is_operation = True
                         break
+
                 if not is_operation:
                     if c.isalpha(): # operand names must start with letter
                         # parse the variable name
                         test_idx = curr_idx + 1
-                        while test_idx < len(raw_infix_expr) and is_valid_operand_char_non_leading(raw_infix_expr[test_idx]):
+                        while test_idx < len(condensed_expr) and is_valid_operand_char_non_leading(condensed_expr[test_idx]):
                             test_idx += 1
 
-                        var_name = raw_infix_expr[curr_idx:test_idx]
+                        var_name = condensed_expr[curr_idx:test_idx]
                         unique_symbol = self.get_unique_symbol(var_name)
-                        raw_infix_expr = raw_infix_expr[:curr_idx] + unique_symbol + raw_infix_expr[test_idx:]
+                        condensed_expr = condensed_expr[:curr_idx] + unique_symbol + condensed_expr[test_idx:]
                     elif c in ["0", "1"]:
                         pass
                     else:
                         log.error("TEMP ERROR MSG: Invalid symbol")
             curr_idx += 1
 
-        # TODO: Check parentheses
-        # Syntax checking is done in a post-processing of the condensed expression
+        # Extensive syntax checking is done in a post-processing of the condensed expression
+        left_paren_pos_stack = []
+        operand_pos_list = []
+        operation_pos_list = []
+        for pos, c in enumerate(condensed_expr):
+            if not c.strip():
+                continue
+            elif c == "(":
+                left_paren_pos_stack.append(pos)
+            elif c == ")":
+                if not left_paren_pos_stack:
+                    # TODO: log message
+                    raise UnbalancedParenException
+                else:
+                    left_paren_pos_stack.pop()
+            elif c in self.get_unique_symbol_list() or c in ["0", "1"]:
+                operand_pos_list.append(pos)
+            else:
+                # assume we replaced correctly above and c must be an operation symbol
+                operation_pos_list.append(pos)
 
-        return without_spaces(raw_infix_expr)
+        if left_paren_pos_stack:
+            # TODO: log message
+            raise UnbalancedParenException
+
+        if len(operand_pos_list) - len(operation_pos_list) != 1:
+            # TODO: log message
+            raise GrammarException
+
+        for operand_pos, operation_pos in itertools.zip_longest(operand_pos_list, operation_pos_list, fillvalue=float("inf")):
+            if operand_pos > operation_pos:
+                # TODO: log message
+                raise GrammarException
+
+        return without_spaces(condensed_expr)
 
     def infix_to_postfix(self, infix_expr):
         """
@@ -131,7 +189,7 @@ class BooleanExpressionWrapper(object):
             infix_expr: The Boolean expression to convert in infix form, as a string.
 
         Returns:
-            A BooleanExpressionWrapper instance filled with the infix and postfix representations of the expression
+            A BooleanEquationWrapper instance filled with the infix and postfix representations of the expression
         """
         stack = []
         postfix = []
@@ -160,39 +218,6 @@ class BooleanExpressionWrapper(object):
         return "".join(postfix)
 
 
-def get_evaluation_result(raw_eq):
-    """
-    This is the main flow for truth table computation.  The flow is divided into several steps:
-        1. Transform the passed equation into the tt schema, as defined in the schema_provider module.
-        2. Split the equation i\nto the result symbol and the infix Boolean expression to be evaluated.
-        3. Extract the input symbols from the Boolean expression.
-        4. Convert the infix expression to its postfix form.
-        5. Evaluate the postfix expression at each possible combination of bool inputs to the expression.
-
-    Args:
-        raw_eq: The infix boolean equation to evaluate, unchanged from the user's original entry.
-
-    Returns:
-        An instance of EvaluationResultWrapper containing the input symbols, output symbol, and list of results of the
-        equation's evaluation for each possible boolean combination of its inputs.
-    """
-    generic_schema_eq = transform_eq_to_generic_schema(raw_eq)
-    output_sym, expr = extract_output_sym_and_expr(generic_schema_eq)
-    # input_syms = sorted(extract_symbols(expr))
-    expr = "A and B"
-    bool_expr_wrapper = infix_to_postfix(expr)
-    postfix_expr = bool_expr_wrapper.postfix_repr
-    input_syms = sorted(bool_expr_wrapper.get_unique_symbol_list())
-
-    eval_result_wrapper = EvaluationResultWrapper(input_syms, output_sym)
-    for input_row in get_sym_input_array(input_syms):
-        expr_to_eval = replace_inputs(postfix_expr, input_syms, input_row)
-        result = eval_postfix_expr(expr_to_eval)
-        eval_result_wrapper.result_list.append(str(result))
-
-    return eval_result_wrapper
-
-
 def extract_output_sym_and_expr(eq):
     """
     Split the passed boolean equation on its equals sign into the output symbol and its equivalent boolean expression.
@@ -212,19 +237,13 @@ def extract_output_sym_and_expr(eq):
     output_and_expr = eq.split("=")
 
     if len(output_and_expr) == 1:
-        log.error("Boolean equation did not contain equals sign (\"=\").\n"
-                  "Cannot continue program execution.")
+        log.error("Boolean equation did not contain equals sign (\"=\"). Cannot continue program execution.")
         raise RuntimeError
     elif len(output_and_expr) > 2:
-        log.error("More than one equals sign (\"=\") found in your equation.\n"
-                  "Cannot continue program execution.")
-        raise RuntimeError 
-    
+        log.error("More than one equals sign (\"=\") found in your equation. Cannot continue program execution.")
+        raise RuntimeError
+
     output_sym, expr = output_and_expr[0], output_and_expr[1]
-    if not is_valid_variable_symbol(output_sym):
-        log.warning("Output symbol did not meet the recommended guidelines for equation symbols. "
-                    "This is probably fine for the output variable, but may cause problems if you followed the same "
-                    "patterns for dependent variable symbols.")
     return output_sym, expr
 
 
@@ -260,16 +279,13 @@ def eval_postfix_expr(expr_to_eval):
     return stack[0]
 
 
-def get_sym_input_array(sym_list):
+def get_symbol_input_array(sym_list):
     return itertools.product(["0", "1"], repeat=len(sym_list))
 
 
 # === Grammar-related rules ============================================================================================
 def is_valid_operand_char_non_leading(c):
     return c == "_" or c.isalnum()
-
-def is_non_item_changing_char(c):
-    pass
 
 
 # === Custom exception types ===========================================================================================
