093e7f967875ca7d5ed96a2581d0c78187b68abf
welchbj <welch18@vt.edu>
2015-12-21 14:17:37 -0500

    Prepared core.py for production use
    Add relevant information to help statements.
    Assign meaning to different error level values.
    Define implemented features.

--- tt/core.py
+++ tt/core.py
@@ -1,44 +1,50 @@
-import sys
-import os
+"""
+A module containing the gateway between the command line interface and tt's underlying functionality.
+"""
+
 import logging as log
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 
-from eqtools import get_evaluation_result
+from eqtools import BooleanEquationWrapper, GrammarError
 from fmttools import TruthTablePrinter
 
 __all__ = ['main']
 __version__ = 0.1
 __date__ = "2015-11-24"
-__updated__ = "2015-11-24"
+__updated__ = "2015-12-21"
 
-def main(argv=None):
-    """The main entry point for the program. Arguments and arguments 
-        descriptions are defined, and the correct functionality is chosen
-        from the user input.    
+
+def main():
     """
-    if argv is None:
-        argv = ["tt", '"F = A and B"']
-    else:
-        sys.argv.extend(argv)
+    The main entry point to the command line application. Sets up the arg parser and chooses which control flow to
+    follow according to user's command line options.
+
+    Returns:
+        The error level:
+            0: Program ran fine.
+            1: Recognized error occurred.
+            2: Unrecognized error occurred.
+    """
+    logging_format = "%(levelname)s: %(message)s"
 
     # program information
     program_source_control_url = "www.github.com/welchbj/tt"
-    program_name = os.path.basename(sys.argv[0])
     program_version = "v{0}".format(str(__version__))
     program_build_date = str(__updated__)
     program_version_message = "{version}, {build}".format(version=program_version, build=program_build_date)
     program_license = "MIT"
     program_author = "Brian Welch"
-    program_desc = """
+    program_desc = """\
     tt is a command line utility for printing truth tables and Karnaugh Maps.
     Here is some pertinent information about the release you are using:
     Version: {version}
     Author: {author}
-    License: {license}""".format(version=program_version,
-                                 author=program_author,
-                                 license=program_license)
-    LOGGING_FORMAT = "%(levelname)s: %(message)s"
+    License: {license}
+    URL: {url}""".format(version=program_version,
+                         author=program_author,
+                         license=program_license,
+                         url=program_source_control_url)
 
     try:
         # Setup argument parser
@@ -48,27 +54,35 @@ def main(argv=None):
                             action="version",
                             version=program_version_message,
                             help="Program version and latest build date")
-        parser.add_argument("--verbose", "-v",
+        parser.add_argument("--verbose",
                             action="store_true",
                             help="Specify verbose output, useful for debugging.")
         parser.add_argument("--kmap", 
                             action="store_true", 
-                            help="Generate kmap of specified boolean equation.")
+                            help="Generate kmap of specified boolean equation.\n"
+                                 "Current unsupported.")
         parser.add_argument("--intermediates", 
                             action="store_true", 
-                            help="Indicates that intermediate boolean expressions should be\n"
+                            help="Indicates that intermediate Boolean expressions should be\n"
                                  "displayed with their own column in the truth table.\n"
-                                 "Not valid with the --kmap option.")
+                                 "Not valid with the --kmap option.\n"
+                                 "Currently unsupported.")
         parser.add_argument(dest="equation", 
                             help="Boolean equation to be analyzed, surrounded with double quotes.\n"
-                                 "Boolean operations are specified using plain Englsh in lowercase\n"
-                                 "For example, you would enter:\n"
-                                 "F=A+B as \"F = A or B\"\n"
-                                 "F=AB as \"F = A and B\"\n"
-                                 "Currently the only operations supported are:\n"
-                                 "(1) and\n"
-                                 "(2) or\n"
-                                 "The equation should be the last argument specified") 
+                                 "Boolean operations can be specified using plain English or their common symbolic equivalents.\n"
+                                 "For example, the two equations:\n"
+                                 "\t(1) \"out = operand_1 and operand_2 or operand_3\"\n"
+                                 "\t(2) \"out = operand_1 && operand_2 || operand_3\"\n"
+                                 "Would evaluate identically.\n"
+                                 "\n"
+                                 "Supported Boolean operations are:\n"
+                                 "\tnot\n"
+                                 "\txor\n"
+                                 "\txnor\n"
+                                 "\tand\n"
+                                 "\tnand\n"
+                                 "\tor\n"
+                                 "\tnor\n")
 
         # Process arguments
         args = parser.parse_args()
@@ -78,33 +92,32 @@ def main(argv=None):
         equation = args.equation
         
         if verbose:
-            log.basicConfig(format=LOGGING_FORMAT, level=log.DEBUG)
+            log.basicConfig(format=logging_format, level=log.DEBUG)
             log.info("Starting verbose output.")
         else:
-            log.basicConfig(format=LOGGING_FORMAT)
+            log.basicConfig(format=logging_format)
         
         if kmap and intermediates:
-            parser.error("--intermediates option is not compatible with kmap generation")
+            # parser.error("--intermediates option is not compatible with kmap generation")
+            raise NotImplementedError("--kmap and --intermediates")
         elif kmap:
-            # TODO
-            pass
+            raise NotImplementedError("--kmap")
         elif intermediates:
-            # TODO
-            pass
+            raise NotImplementedError("--intermediates")
         else: # default to truth table generation
-            eval_result = get_evaluation_result(equation)
+            eval_result = BooleanEquationWrapper(equation).get_evaluation_result()
             tt_printer = TruthTablePrinter(eval_result)
             tt_printer.print_tt()
 
     except KeyboardInterrupt:
         return 0
-    except RuntimeError:
+    except GrammarError as e:
+        e.log()
         return 1
-    #except Exception as e:
-    #    indent = len(program_name) * " "
-    #    print("An unexpected error occurred.\n",
-    #          "Please notify the project maintainer at" + program_source_control_url + "\n",
-    #          "Error info:\n",
-    #          program_name + ": " + repr(e) + "\n",
-    #          "for help use --help", sep="")
-    #    return 2
+    except NotImplementedError as e:
+        log.error("Tried to use a feature that is not yet implemented: " + str(e))
+        return 1
+    except Exception as e:
+        log.critical("An unknown error occurred. Cannot continue program execution.")
+        log.critical(str(e))
+        return 2
