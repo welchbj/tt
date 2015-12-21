fd29c9e523f4632cc253d750bcd0e7fde0468436
welchbj <welch18@vt.edu>
2015-12-21 17:59:20 -0500

    Remove unused functions

--- tt/utils.py
+++ tt/utils.py
@@ -1,9 +1,2 @@
 def without_spaces(the_str):
     return "".join(the_str.split())
-
-def listwise_to_str(the_list):
-    return list(map(str, the_list))
-
-def str_replace_range(the_string, start, end, replace_string):
-    # replace
-    return the_string[:start] + replace_string + the_string[(end+1):]
