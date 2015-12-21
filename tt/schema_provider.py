4b275261a7c2633ac503b118002a950726822db8
welchbj <welch18@vt.edu>
2015-12-21 17:49:57 -0500

    Reorganize schema_provider.py
    Refactor Boolean functions from assigned lambdas to free functions
    Separate sections of file by purpose

--- tt/schema_provider.py
+++ tt/schema_provider.py
@@ -13,6 +13,7 @@ __all__ = ["BooleanOperator",
            "SYM_XOR"]
 
 
+# === Wrapper Classes ==================================================================================================
 class BooleanOperator(object):
     def __init__(self, precedence_in, bool_func_in, *args):
         self.precedence = precedence_in
@@ -22,24 +23,40 @@ class BooleanOperator(object):
     def result(self, a, b):
         return self.bool_func(a, b)
 
-# functions called in Boolean expression evaluation;
-# a and b should always be passed as ints to these functions
-tt_and = lambda a, b: int(a and b)
-tt_nand = lambda a, b: int(not tt_and(a, b))
-tt_or = lambda a, b: int(a or b)
-tt_nor = lambda a, b: int(not tt_or(a, b))
-tt_xor = lambda a, b: int((not a and b) or (a and not b))
-tt_xnor = lambda a, b: int(not tt_xor(a, b))
+
+# === Boolean Functions ================================================================================================
+def tt_and(a, b):
+    return int(a and b)
 
 
-def uncallable(a, b):
+def tt_nand(a, b):
+    return int(not(a and b))
+
+
+def tt_or(a, b):
+    return int(a or b)
+
+
+def tt_nor(a, b):
+    return int(not(a or b))
+
+
+def tt_xor(a, b):
+    return int(a != b)
+
+
+def tt_xnor(a, b):
+    return int(a == b)
+
+
+def tt_uncallable(a, b):
     log.critical("Boolean not function was explicitly called. "
-              "This should not happen; not is implemented as xor with 1. "
-              "Cannot continue program execution.")
+                 "This should not happen; not is implemented as xor with 1. "
+                 "Cannot continue program execution.")
     raise RuntimeError
 
-# Define schema information
 
+# === Schema Information ===============================================================================================
 precedence = {
     "ZERO" : 0,
     "LOW" : 1,
@@ -55,9 +72,8 @@ SYM_NAND = "$"
 SYM_OR = "|"
 SYM_NOR = "%"
 
-# TODO: possibly allow users to make their own precedence maps in the future
 schema = {
-    SYM_NOT : BooleanOperator(precedence["HIGH"], uncallable, "not", "NOT", "~", "!"),
+    SYM_NOT : BooleanOperator(precedence["HIGH"], tt_uncallable, "not", "NOT", "~", "!"),
     SYM_XOR : BooleanOperator(precedence["MEDIUM"], tt_xor, "xor", "XOR"),
     SYM_XNOR : BooleanOperator(precedence["MEDIUM"], tt_xnor, "xnor", "XNOR", "nxor", "NXOR"),
     SYM_AND : BooleanOperator(precedence["LOW"], tt_and, "and", "AND", "&&", "&", "/\\"),
