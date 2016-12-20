70327da557aa7e9f632becd2f7186a304294c5b8
welchbj <welch18@vt.edu>
2016-12-19 19:43:18 -0500

    Refactor argparse flow for CLI

--- tt/cli/core.py
+++ tt/cli/core.py
@@ -6,10 +6,45 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 
 from ..errors import GrammarError
 from ..expressions import BooleanExpression
+from ..tables import TruthTable
 from .utils import print_err, print_info
 
 
-__version__ = 0.4
+__version__ = '0.4.1'
+
+
+def _add_expression_arg(parser):
+    """Add the expression argument to a parser."""
+    parser.add_argument(
+        'expression',
+        nargs='*',
+        help=(
+            'Boolean expression to process; surround it in quotes to\n'
+            'avoid escaping control characters in your terminal'))
+
+
+def _tokens(opts):
+    """Run the `tokens` command."""
+    b = BooleanExpression(opts.expression)
+    print_info('\n'.join(b.tokens))
+
+
+def _postfix_tokens(opts):
+    """Run the `postfix-tokens` command."""
+    b = BooleanExpression(opts.expression)
+    print_info('\n'.join(b.postfix_tokens))
+
+
+def _tree(opts):
+    """Run the `tree` command."""
+    b = BooleanExpression(opts.expression)
+    print_info(b.tree)
+
+
+def _table(opts):
+    """Run the `table` command."""
+    t = TruthTable(opts.expression)
+    print_info(t)
 
 
 def get_parsed_args(args=None):
@@ -39,30 +74,35 @@ def get_parsed_args(args=None):
         version='v'+str(__version__),
         help='program version')
 
-    parser.add_argument(
-        '--tokens',
-        action='store_true',
-        dest='tokens',
-        help='print the parsed tokens of the passed expression')
+    sub_parsers = parser.add_subparsers(help='the tt command to run')
 
-    parser.add_argument(
-        '--postfix-tokens',
-        action='store_true',
-        dest='postfix_tokens',
-        help='print the tokens of the postfix form of the passed expression')
+    # tokens sub-parser
+    parser_tokens = sub_parsers.add_parser(
+        'tokens',
+        help='print the expression\'s tokens in order of appearance')
+    _add_expression_arg(parser_tokens)
+    parser_tokens.set_defaults(func=_tokens)
 
-    parser.add_argument(
-        '--tree',
-        action='store_true',
-        dest='tree',
-        help='print the tree representation of the passed expression')
+    # postfix-tokens sub-parser
+    parser_postfix_tokens = sub_parsers.add_parser(
+        'postfix-tokens',
+        help='print the expression\'s tokens in postfix order')
+    _add_expression_arg(parser_postfix_tokens)
+    parser_postfix_tokens.set_defaults(func=_postfix_tokens)
 
-    parser.add_argument(
-        'expression',
-        nargs='*',
-        help=(
-            'Boolean expression to process; surround it in quotes to\n'
-            'avoid escaping control characters in your terminal'))
+    # tree sub-parser
+    parser_tree = sub_parsers.add_parser(
+        'tree',
+        help='print the expression\'s tree representation')
+    _add_expression_arg(parser_tree)
+    parser_tree.set_defaults(func=_tree)
+
+    # table sub-parser
+    parser_table = sub_parsers.add_parser(
+        'table',
+        help='print the expression\'s truth table')
+    _add_expression_arg(parser_table)
+    parser_table.set_defaults(func=_table)
 
     if args is None:
         args = sys.argv[1:]
@@ -86,22 +126,13 @@ def main(args=None):
             args = sys.argv[1:]
 
         opts = get_parsed_args(args)
+        func = getattr(opts, 'func', None)
+        if func is None:
+            print_err('No command specified; use `tt --help` for options.')
+            return 2
 
-        tokens = opts.tokens
-        postfix_tokens = opts.postfix_tokens
-        tree = opts.tree
-        expression = ' '.join(opts.expression)
-
-        b = BooleanExpression(expression)
-
-        if tokens:
-            print_info('\n'.join(b.tokens))
-
-        if postfix_tokens:
-            print_info('\n'.join(b.postfix_tokens))
-
-        if tree:
-            print_info(b.expr_tree)
+        opts.expression = ' '.join(opts.expression)
+        opts.func(opts)
 
         return 0
     except GrammarError as e:
