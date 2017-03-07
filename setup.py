313b3c8b659ca41cb6a023e1656f61356ccc7439
welchbj <welch18@vt.edu>
2017-03-06 19:05:33 -0500

    Add Python 3.6 identifier to setup.py

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ tt_classifiers = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Utilities'
 ]
 
