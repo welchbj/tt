7322efbb479fcb64b808b74ff0fa571e5a22d57a
welchbj <welch18@vt.edu>
2017-03-07 16:31:26 -0500

    Add Python 2.7, 3.3, and 3.4 identifiers back into setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,9 @@ tt_classifiers = [
     'Operating System :: MacOS :: MacOS X',
     'Operating System :: Microsoft :: Windows',
     'Operating System :: POSIX',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Topic :: Utilities'
