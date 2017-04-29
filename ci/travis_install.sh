1bd483986e1d257d2cd03b892c1e4ddad3c782ed
welchbj <welch18@vt.edu>
2017-04-29 18:38:26 -0400

    Add brew install of python3 when Py version is 3.x for Travis OSX builds

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    if [[ ${PYENV_VERSION:0:1} == "3" ]]; then
+        # we need the Python dev headers
+        brew install python3
+    fi
+
     export PYENV_VERSION_STRING="Python ${PYENV_VERSION}"
     wget https://github.com/praekeltfoundation/travis-pyenv/releases/download/0.3.0/setup-pyenv.sh
     source setup-pyenv.sh
