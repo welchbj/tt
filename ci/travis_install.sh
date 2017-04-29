b7d864e1e9dfcf27f7cbcf815052a5127030776f
welchbj <welch18@vt.edu>
2017-04-29 19:23:51 -0400

    Add which cmd to determine if python is being determined correctly

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -6,6 +6,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     source setup-pyenv.sh
 fi
 
+which python
+
 if [[ $RUN_FLAKE ]]; then python -m pip install flake8; fi
 python -m pip install wheel
 CC=gcc python setup.py develop
