9dfea6eda65ab7c940f35993737272111603f3f8
welchbj <welch18@vt.edu>
2017-04-29 17:51:01 -0400

    Add experimental support for Travis 64-bit OSX builds

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    brew update
+    brew install openssl readline xz
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+
+    pyenv install $TT_PY_VER
+    export PYENV_VERSION=$TT_PY_VER
+    export PATH="$HOME/.pyenv/shims:${PATH}"
+
+    pyenv-virtualenv venv
+    source venv/bin/activate
+
+    python --version
+fi
+
 if [[ $RUN_FLAKE ]]; then python -m pip install flake8; fi
 python -m pip install wheel
-
-if [[ $TT_32_BIT ]]; then
-    sudo apt-get install g++-multilib
-    CFLAGS=-m32 LDFLAGS=-m32 python setup.py develop
-else
-    python setup.py develop
-fi
+python setup.py develop
