6f13257e3ded90ebc32567773ed7a18a48c12369
welchbj <welch18@vt.edu>
2017-04-29 19:12:32 -0400

    Try updating cached bash python in OSX travis builds

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    export LDFLAGS="-Wl,-export_dynamic"
     export PYENV_VERSION_STRING="Python ${PYENV_VERSION}"
     wget https://github.com/praekeltfoundation/travis-pyenv/releases/download/0.3.0/setup-pyenv.sh
     source setup-pyenv.sh
+    hash -r python
 fi
 
 if [[ $RUN_FLAKE ]]; then python -m pip install flake8; fi
