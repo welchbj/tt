1faa8c7e61f6ef52dbc0c1dd224fff63bc27ff36
welchbj <welch18@vt.edu>
2017-04-30 10:32:04 -0400

    Update pyenv flow on travis and separate Travis script into its own file

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
 
+set -e
+set -x
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    export PYENV_VERSION_STRING="Python ${PYENV_VERSION}"
-    wget https://github.com/praekeltfoundation/travis-pyenv/releases/download/0.3.0/setup-pyenv.sh
-    source setup-pyenv.sh
+    brew update || brew update
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+
+    case "${TT_PY_VER}" in
+        py27)
+            pyenv install 2.7.10
+            pyenv virtualenv 2.7.10 tt-dev
+            ;;
+
+        py33)
+            pyenv install 3.3.6
+            pyenv virtualenv 3.3.6 tt-dev
+            ;;
+
+        py34)
+            pyenv install 3.4.3
+            pyenv virtualenv 3.4.3 tt-dev
+            ;;
+
+        py35)
+            pyenv install 3.5.3
+            pyenv virtualenv 3.5.3 tt-dev
+            ;;
+
+        py36)
+            pyenv install 3.6.1
+            pyenv virtualenv 3.6.1 tt-dev
+            ;;
+    esac
+
+    pyenv rehash
+    pyenv activate tt-dev
 fi
 
 which python
-
-if [[ $RUN_FLAKE ]]; then python -m pip install flake8; fi
 python -m pip install wheel
-CC=gcc python setup.py develop
+if [[ $RUN_FLAKE ]]; then python -m pip install flake8; fi
